6bfc3cf6a169c3a68f4a7bb9f42da3768c0b8cb2
Chaitanya S Bidkar <85572811+ChaitanyaBidkar@users.noreply.github.com>
2021-11-24 17:17:12 +0530

    done1

--- app-practice/app_custom_storefront_practice/cartridge/client/default/js/productTile.js
+++ app-practice/app_custom_storefront_practice/cartridge/client/default/js/productTile.js
@@ -5,3 +5,17 @@ var processInclude = require('./util');
 $(document).ready(function () {
     processInclude(require('./product/quickView'));
 });
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
