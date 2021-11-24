369667e9125b1b9b0e73d0d89c4bfd26c11abe5e
Chaitanya S Bidkar <85572811+ChaitanyaBidkar@users.noreply.github.com>
2021-11-24 15:58:45 +0530

    Accordian
    done

--- app-practice/app_custom_storefront_practice/cartridge/client/default/js/productTile.js
+++ app-practice/app_custom_storefront_practice/cartridge/client/default/js/productTile.js
@@ -5,3 +5,19 @@ var processInclude = require('./util');
 $(document).ready(function () {
     processInclude(require('./product/quickView'));
 });
+
+// accordian start here
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
